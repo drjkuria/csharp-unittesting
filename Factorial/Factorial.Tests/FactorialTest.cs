b6b6d9a4d5c138ea6aecc1db30f20f250b01fbe2
Jonah Kuria <jonah.kuria@hotmail.com>
2015-05-20 16:40:38 +0200

    Add test for negative numbers

--- Factorial/Factorial.Tests/FactorialTest.cs
+++ Factorial/Factorial.Tests/FactorialTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Factorial;
+using NUnit.Framework;
 
 namespace Factorial.Tests
 {
+    [TestFixture]
     public class FactorialTest
     {
+        [Test]
+        [ExpectedException(typeof(NegativeNumberFactorial))]
+        public void TestFactorialOfANegativeNumber()
+        {
+
+        }
     }
 }
