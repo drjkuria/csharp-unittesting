e68de20a30c9bc65d616ad78ae44b56f32d917ab
Jonah Kuria <jonah.kuria@hotmail.com>
2015-05-20 22:20:43 +0200

    add test for compliment of a chance

--- Probability/Probability.Tests/ChanceTest.cs
+++ Probability/Probability.Tests/ChanceTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Probability;
+using NUnit.Framework;
 
 namespace Probability.Tests
 {
+    [TestFixture]
     public class ChanceTest
     {
+        [Test]
+        public void TestForCompliment()
+        {
+            Chance thirtyPercentChance = new Chance(new decimal(0.3));
+            Assert.AreEqual(new Chance(new decimal(0.7)), thirtyPercentChance.Not());
+        }
     }
 }
---
+++ Probability/Probability/Chance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Probability
+{
+    public class Chance
+    {
+        private decimal value;
+
+        public Chance(decimal value)
+        {
+            this.value = value;
+        }
+
+        public Chance Not()
+        {
+            return null;
+        }
+    }
+}
