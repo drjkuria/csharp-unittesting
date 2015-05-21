8d4414363c8c0169b4a8b40527f299fe3230f844
Jonah Kuria <jonah.kuria@hotmail.com>
2015-05-21 09:50:21 +0200

    add perimeter test

--- Rectangle/Rectangle.Tests/RectangleTest.cs
+++ Rectangle/Rectangle.Tests/RectangleTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rectangle;
+using NUnit.Framework;
 
 namespace Rectangle.Tests
 {
-    class RectangleTest
+    [TestFixture]
+    public class RectangleTest
     {
+        [Test]
+        public void TestPerimeter()
+        {
+            Rectangle rectangle = new Rectangle(5, 4);
+            Assert.AreEqual(18, rectangle.Perimeter());
+        }
     }
 }
--- Rectangle/Rectangle/Rectangle.cs
+++ Rectangle/Rectangle/Rectangle.cs
@@ -8,5 +8,30 @@ namespace Rectangle
 {
     public class Rectangle
     {
+        private int length;
+        private int width;
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int Perimeter()
+        {
+            return 2 * (length + width);
+        }
     }
 }
